17dda3372da9b78c9e345e9c090f9c6daf6845aa
andywong31 <wongandy31@gmail.com>
2021-07-03 19:16:35 +0800

    created home controller

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+  <div class="flex justify-center">
+    <div class="w-6/12 bg-white p-5">
+      <h1 class="font-bold mb-5">Introducing Tasker! </h1>
+
+      <p>Tasker is a web app where admins can assign tasks to users and keep track of their progress!</p>
+    </div>
+  </div>
+@endsection
+</div>
---
+++ resources/views/partials/_navbar.blade.php
@@ -0,0 +1,12 @@
+<nav class=" bg-blue-500">
+  <ul class="flex justify-end text-white">
+    <li class="m-5"><a href="{{ route('/') }}">Home</a></li>
+    <li class="m-5"><a href="">Dashboard</a></li>
+    <li class="m-5"><a href="">Tasks</a></li>
+    <li class="m-5"><a href="">Users</a></li>
+    <li class="m-5"><a href="">Login</a></li>
+    <li class="m-5"><a href="">Register</a></li>
+    <li class="m-5"><a href="">Andy</a></li>
+    <li class="m-5"><a href="">Logout</a></li>
+  </ul>
+</nav>
